9225bd25da3bca4055183132a147c7195067c7ed
bodelha <bodelha@gmail.com>
2024-03-17 21:08:08 -0300

    exercicio 9 lista 2

--- br.fatec.tp2.exe2/src/AppExercicio9.java
+++ br.fatec.tp2.exe2/src/AppExercicio9.java
@@ -9,13 +9,12 @@ public class AppExercicio9 {
         ArrayList<Integer> controle = new ArrayList<Integer>();
 
         for (int i = 0; i < 5; i++) {
-            // System.out.println("Linha " + (i + 1) + ":");
             for (int j = 0; j < 5; j++) {
-                int numero = random.nextInt(99);
+                int numero = random.nextInt(98) + 1;
                 if (!controle.contains(numero)) {
                     controle.add(numero);
                 }
-                System.out.printf("%3d ", numero);
+                System.out.printf("%2d ", numero);
             }
             System.out.println();
         }
