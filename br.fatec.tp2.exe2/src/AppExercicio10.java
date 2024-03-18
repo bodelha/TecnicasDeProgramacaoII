962f1c91a1861d5cf6a276b66914f579dc811a2d
bodelha <bodelha@gmail.com>
2024-03-17 21:31:17 -0300

    exercicio 10 lista 2

--- br.fatec.tp2.exe2/src/AppExercicio10.java
+++ br.fatec.tp2.exe2/src/AppExercicio10.java
@@ -5,18 +5,12 @@ public class AppExercicio10 {
 
     public static void main(String[] args) {
 
-        double volume = volume(1);
-
-        if (volume == 4.1888) {
-            System.out.println("Volume foi adequadamente calculado");
-        }
-
-        System.out.println("Insira valor para raio");
+        System.out.println("Insira valor para raio (raio=1 deve retornar 4,1888)");
         double raio = scanner.nextDouble();
-        volume(raio);
+        calcularVolume(raio);
     }
 
-    public static double volume(double raio) {
+    public static double calcularVolume(double raio) {
         double volume = (4.0 / 3.0) * Math.PI * Math.pow(raio, 3);
         System.out.printf("Raio:  %.4f\nVolume: %.4f\n", raio, volume);
         return volume;
